fbfccbfe5f6062bce2d30929c7e53e07213a8cb4
Hudi304 <hudisteanum@yahoo.com>
2023-08-23 21:45:08 +0300

    Remove rematch store

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import { ThemeProvider } from './components/theme/themeProvider'
-import { ModalProvider } from './context/modal-context'
-import './index.scss'
-import store from './store'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
+import { ThemeProvider } from "./components/theme/themeProvider"
+import { ModalProvider } from "./context/modal-context"
+import App from "./App"
+import "./index.scss"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
-        <ModalProvider>
-          <ThemeProvider>
-            <App />
-          </ThemeProvider>
-        </ModalProvider>
-      </Provider>
+      <ModalProvider>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </ModalProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 )
